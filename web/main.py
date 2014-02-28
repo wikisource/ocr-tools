ebd5024ec7e47ac85a41bf4d1ff98df15eb8cac3
Guillaume Horel <guillaume.horel@gmail.com>
2014-02-28 00:37:05 -0500

    add a handler for images, everything is dynamic!

--- web/main.py
+++ web/main.py
@@ -3,7 +3,8 @@ from tornado.web import RequestHandler, Application
 import tornado.ioloop
 from settings import settings
 import utils
-
+from parsedjvutext import image_from_book
+import io
 
 class MainHandler(RequestHandler):
 
@@ -12,10 +13,21 @@ class MainHandler(RequestHandler):
         self.render("index.html", page_number=page_number,
                     areas=areas, words=words)
 
+class ImageHandler(RequestHandler):
+
+    def get(self, page_number):
+        im = image_from_book("../Villiers_de_L'Isle-Adam_-_Tribulat_Bonhomet,_1908.djvu", int(page_number))
+        self.set_header('Content-Type', 'image/png')
+        img_buff = io.BytesIO()
+        im.save(img_buff, format="PNG")
+        img_buff.seek(0)
+        self.write(img_buff.read())
+        self.finish()
 
 application = Application([
-    (r'/(\d+)/?', MainHandler)
-], **settings)
+    (r'/(\d+)/?', MainHandler),
+    (r'/(\d+)\.png/?', ImageHandler)]
+    , **settings)
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)
--- web/utils.py
+++ web/utils.py
@@ -7,8 +7,7 @@ def gen_html(book, page_number):
     d = parse_book(book, page=int(page_number), html=True)
     if d[0]:
         words, coords = zip(*d[0])
-
-    return (list(enumerate(coords)), list(enumerate(words)))
+        return (list(enumerate(coords)), list(enumerate(words)))
 
 if __name__ == "__main__":
     gen_html(*sys.argv[1:3])
