74604d7b8ae98b125f1c800da753f8ab67474eb5
Guillaume Horel <guillaume.horel@gmail.com>
2014-09-07 16:32:08 -0400

    cleanup

--- string_utils.py
+++ string_utils.py
@@ -31,31 +31,6 @@ def cut(word, left, right):
     l = [aux(i) for i in xrange(len(word) + 1)]
     return min(l, key=lambda x: x[2])[:2]
 
-def LCS(X, Y):
-    m = len(X)
-    n = len(Y)
-    # An (m+1) times (n+1) matrix
-    C = [[0] * (n+1) for i in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                C[i][j] = C[i-1][j-1] + 1
-            else:
-                C[i][j] = max(C[i][j-1], C[i-1][j])
-    return C
-
-def printDiff(C, X, Y, i, j):
-    if i > 0 and j > 0 and X[i-1] == Y[j-1]:
-        printDiff(C, X, Y, i-1, j-1)
-        print "  " + X[i-1]
-    else:
-        if j > 0 and (i == 0 or C[i][j-1] >= C[i-1][j]):
-            printDiff(C, X, Y, i, j-1)
-            print "+ " + Y[j-1]
-        elif i > 0 and (j == 0 or C[i][j-1] < C[i-1][j]):
-            printDiff(C, X, Y, i-1, j)
-            print "- " + X[i-1]
-
 def join_ocr_words(l, c):
     m = list(l)
     if len(l) >= 2 and c[-2][2] > c[-1][0] and (not l[-2][-1].isalnum()):
